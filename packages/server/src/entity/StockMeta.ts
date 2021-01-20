29f13f3286ac604da97ba1d34aa1919996dcf5a7
chesedi <a02t02@gmail.com>
2021-01-20 17:23:10 +0900

    CreateStock, StockMeta, SectorWeighting, HistoricalData tables

---
+++ packages/server/src/entity/StockMeta.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  Index,
+} from 'typeorm'
+import { Stock } from './Stock'
+
+@Entity({
+  name: 'stock_meta',
+})
+export class StockMeta {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    type: 'double',
+  })
+  price: number
+
+  @Column({
+    type: 'double',
+  })
+  market_cap: number
+
+  @Column()
+  is_tracking: boolean
+
+  @OneToOne((type) => Stock, (stock) => stock.stockMeta, { cascade: true })
+  @JoinColumn({ name: 'stock_id' })
+  stock: Stock
+}
